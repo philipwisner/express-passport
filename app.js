0a6c6e97e28aca9966ca95b2eda6258a4db019ef
Philip Wisner <philipwisner@gmail.com>
2018-05-29 23:04:05 -0700

    update auth route

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/auth', auth);
+app.use('/', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,92 +1,43 @@
-var express = require('express');
-var passport = require('passport');
-var router = express.Router();
-var bcrypt = require('bcrypt');
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const {
+  ensureLoggedIn,
+  ensureLoggedOut
+} = require('connect-ensure-login');
 
-var User = require('../models/user').User;
+const User = require("../models/user");
 
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return response.notFound(req, res);
-    }
-    req.login(user, (err) => {
-      if (err) {
-        return next(err);
-      }
-      return response.data(req, res, req.user);
-    });
-  })(req, res, next);
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+
+//signIn
+router.get('/login', ensureLoggedOut(), (req, res) => {
+  res.render('auth/login');s
 });
 
-router.post('/signup', (req, res, next) => {
-  const {
-    name,
-    email,
-    password
-  } = req.body;
+router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
+  successReturnToOrRedirect: 'index',
+  failureRedirect: '/login',
+  passReqToCallback: true
+}));
 
-  if (!name) {
-    return response.unprocessable(req, res, 'Missing mandatory field "Name".');
-  }
-  if (!password) {
-    return response.unprocessable(req, res, 'Missing mandatory field "Password".');
-  }
-  if (!email) {
-    return response.unprocessable(req, res, 'Missing mandatory field "Email".');
-  }
-
-  User.findOne({
-    email
-  }, 'email', (err, userExists) => {
-    if (err) {
-      return next(err);
-    }
-    if (userExists) {
-      return response.unprocessable(req, res, 'email already in use.');
-    }
-
-    const salt = bcrypt.genSaltSync(10);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = User({
-      email,
-      name,
-      password: hashPass
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        return next(err);
-      }
-      req.login(newUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-        return response.data(req, res, newUser.asData());
-      });
-    });
-  });
+//signUp
+router.get('/signup', ensureLoggedOut(), (req, res) => {
+  res.render('auth/signup');
 });
 
-router.post('/logout', (req, res) => {
+router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+  successReturnToOrRedirect: 'main',
+  failureRedirect: '/signup',
+  passReqToCallback: true
+}));
+
+router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
   req.logout();
-  return response.ok(req, res);
+  res.redirect('/');
 });
-
-
-router.get('/me', (req, res) => {
-  if (req.isAuthenticated()) {
-    return User.findById(req.user._id, (err, user) => {
-      return response.data(req, res, user.asData());
-    });
-  }
-
-  return response.notFound(req, res);
-});
-
 
 module.exports = router;
