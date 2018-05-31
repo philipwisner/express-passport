4e8683ad78c12828f104b63494cf2ce67cc6b020
Philip Wisner <philipwisner@gmail.com>
2018-05-31 00:03:40 -0400

    added fb and google passport

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,6 @@
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 var passport = require("passport");
 var bcrypt = require('bcrypt');
 var bcryptSalt = 10;
@@ -7,88 +9,154 @@ var User = require("../models/user");
 // load up the user model
 
 // expose this function to our app using module.exports
+// expose this function to our app using module.exports
 module.exports = (passport) => {
 
-        // =========================================================================
-        // passport session setup ==================================================
-        // =========================================================================
-        // required for persistent login sessions
-        // passport needs ability to serialize and unserialize users out of session
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
 
-        // used to serialize the user for the session
-        passport.serializeUser((user, cb) => {
-            cb(null, user.id);
+    // used to serialize the user for the session
+    passport.serializeUser((user, cb) => {
+        cb(null, user.id);
+    });
+
+    passport.deserializeUser((id, cb) => {
+        User.findById(id, (err, user) => {
+            if (err) {
+                return cb(err);
+            }
+            cb(null, user);
         });
+    });
 
-        passport.deserializeUser((id, cb) => {
-            User.findById(id, (err, user) => {
-                if (err) {
-                    return cb(err);
-                }
-                cb(null, user);
-            });
+
+    passport.use('local-login', new LocalStrategy((username, password, next) => {
+        User.findOne({
+            username
+        }, (err, user) => {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                return next(null, false, {
+                    message: "Incorrect username"
+                });
+            }
+            if (!bcrypt.compareSync(password, user.password)) {
+                return next(null, false, {
+                    message: "Incorrect password"
+                });
+            }
+
+            return next(null, user);
         });
+    }));
 
+    passport.use('local-signup', new LocalStrategy({
+            passReqToCallback: true
+        },
+        (req, username, password, next) => {
+            process.nextTick(() => {
+                User.findOne({
+                    'username': username
+                }, (err, user) => {
+                    if (err) {
+                        return next(err);
+                    }
 
-        passport.use('local-login', new LocalStrategy((username, password, next) => {
-            User.findOne({
-                username
-            }, (err, user) => {
-                if (err) {
-                    return next(err);
-                }
-                if (!user) {
-                    return next(null, false, {
-                        message: "Incorrect username"
-                    });
-                }
-                if (!bcrypt.compareSync(password, user.password)) {
-                    return next(null, false, {
-                        message: "Incorrect password"
-                    });
-                }
+                    if (user) {
+                        return next(null, false, {
+                            message: "Username already exists"
+                        });
+                    } else {
+                        const {
+                            username,
+                            email,
+                            password
+                        } = req.body;
+                        const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                        const newUser = new User({
+                            username,
+                            email,
+                            password: hashPass
+                        });
 
-                return next(null, user);
+                        newUser.save((err) => {
+                            if (err) {
+                                next(err);
+                            }
+                            return next(null, newUser);
+                        });
+                    }
+                });
             });
         }));
 
-        passport.use('local-signup', new LocalStrategy({
-                        passReqToCallback: true
-                    },
-                    (req, username, password, next) => {
-                        process.nextTick(() => {
-                            User.findOne({
-                                'username': username
-                            }, (err, user) => {
-                                if (err) {
-                                    return next(err);
-                                }
+    //FACEBOOK STRATEGY
+    passport.use(new FacebookStrategy({
+        clientID: "857307061101319",
+        clientSecret: "6a3eace08f83082fe80681f9078c0105",
+        callbackURL: "/auth/facebook/callback",
+        profileFields: ['id', 'displayName', 'email']
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({
+            facebookID: profile.id
+        }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (user) {
+                return done(null, user);
+            }
 
-                                if (user) {
-                                    return next(null, false, {
-                                        message: "Username already exists"
-                                    });
-                                } else {
-                                    var {
-                                        username,
-                                        email,
-                                        password
-                                    } = req.body;
-                                    var hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-                                    var newUser = new User({
-                                        username,
-                                        email,
-                                        password: hashPass
-                                    });
+            const newUser = new User({
+                facebookID: profile.id,
+                username: profile.displayName,
+                email: profile.emails[0].value
+            });
+            newUser.save((err) => {
+                if (err) {
+                    return done(err);
+                }
+                done(null, newUser);
+            });
+        });
+    }));
 
-                                    newUser.save((err) => {
-                                        if (err) {
-                                            next(err);
-                                        }
-                                        return next(null, newUser);
-                                    });
-                                }
-                            });
-                        });
-                    }));
-                };
+
+    //GOOGLE STRATEGY
+    passport.use(new GoogleStrategy({
+        clientID: "881861196583-d8gjkkped1kgukjsg9cb6eedqja3k8c0.apps.googleusercontent.com",
+        clientSecret: "JwlD-62nOiJLnODXHrWR6ftV",
+        callbackURL: "/auth/google/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({
+            googleID: profile.id
+        }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (user) {
+                return done(null, user);
+            }
+
+            const newUser = new User({
+                googleID: profile.id,
+                username: profile.name.givenName + " " + profile.name.familyName,
+                email: profile.emails[0].value
+            });
+
+            newUser.save((err) => {
+                if (err) {
+                    return done(err);
+                }
+                done(null, newUser);
+            });
+        });
+
+    }));
+
+};
--- models/user.js
+++ models/user.js
@@ -1,42 +1,25 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Your name is required']
-    },
+var UserSchema = new Schema({
     email: {
         type: String,
-        required: [true, 'Email is required'],
-        index: { unique: true }
+        index: {
+            unique: true
+        }
     },
     password: {
-        type: String,
-        required: [true, 'Password is required']
-    }
+        type: String
+    },
+    username: {
+        type: String
+    },
+    facebookID: String,
+    googleID: String
 });
 
-UserSchema.methods.generateHash = function (password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-UserSchema.methods.validPassword = function (password) {
-    return bcrypt.compareSync(password, this.password);
-};
-
-UserSchema.methods.asData = function () {
-    return {
-        id: this._id,
-        name: this.name,
-        email: this.email
-    }
-};
+var User = mongoose.model('User', UserSchema);
 
 
-const User = mongoose.model('User', UserSchema);
 
-module.exports = {
-    User
-};
+module.exports = User;
