aff97151f05b66f01486b1aa18b665254317ebd6
Philip Wisner <philipwisner@gmail.com>
2018-05-31 00:00:08 -0400

    updates

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var expressLayouts = require('express-ejs-layouts');
 var passport = require('passport');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
@@ -12,12 +13,10 @@ var passport = require('passport');
 //PASSPORT FILE
 var configure = require('./config/passport');
 
-//ROUTES
-var index = require('./routes/index');
-var auth = require('./routes/auth');
-
+//CREATE APP
 var app = express();
 
+//MONGO SETUP
 mongoose.connect('mongodb://localhost:27017/passport');
 
 app.use(session({
@@ -29,26 +28,45 @@ app.use(session({
   })
 }));
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+//PASSPORT
 configure(passport);
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 
+//MIDDLEWARE
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use((req, res, next) => {
+  if (typeof (req.user) !== "undefined") {
+    res.locals.userSignedIn = true;
+  } else {
+    res.locals.userSignedIn = false;
+  }
+  next();
+});
+
+
+//ROUTES
+var index = require('./routes/index');
+var auth = require('./routes/auth');
+
 app.use('/', index);
 app.use('/', auth);
 
+
+
+//ERROR HANDLING
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
